f8522d5930b280513659c1278abedc41b9175bc6
anagd <andelamo@myuax.es>
2023-10-09 14:51:48 +0200

    Mandelbrot concurrente

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +9,26 @@ public class Main {
             JFrame frame = new JFrame("Mandelbrot Set");
             Mandelbrot mandelbrotSet = new Mandelbrot();
             frame.add(mandelbrotSet);
+
+            // Agregar un componente Spinner para seleccionar el número de trabajadores
+            SpinnerModel spinnerModel = new SpinnerNumberModel(1, 1, 8, 1); // Rango de 1 a 8 trabajadores
+            JSpinner spinner = new JSpinner(spinnerModel);
+            JButton startButton = new JButton("Start");
+
+            startButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    int numWorkers = (int) spinner.getValue();
+                    mandelbrotSet.calculateMandelbrot(numWorkers);
+                }
+            });
+
+            JPanel controlPanel = new JPanel();
+            controlPanel.add(new JLabel("Number of Workers: "));
+            controlPanel.add(spinner);
+            controlPanel.add(startButton);
+
+            frame.add(controlPanel, BorderLayout.NORTH);
+
             frame.pack();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLocationRelativeTo(null);
@@ -13,3 +36,4 @@ public class Main {
         });
     }
 }
+
--- src/main/java/Mandelbrot.java
+++ src/main/java/Mandelbrot.java
@@ -1,48 +1,75 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Mandelbrot extends JPanel {
     private static final int ANCHO = 800;
     private static final int ALTO = 800;
     private static final int MAX_ITER = 1000;
 
-    public Mandelbrot(){
+    public Mandelbrot() {
         setPreferredSize(new java.awt.Dimension(ANCHO, ALTO));
     }
 
-    @Override
-    protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        imprimirMandelbrot(g);
-    }
-
-    private void imprimirMandelbrot(Graphics g){
+    public void calculateMandelbrot(int numWorkers) {
         BufferedImage image = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_INT_RGB);
 
-        for(int x = 0; x < ANCHO; x++){
-            for(int y = 0; y < ALTO; y++){
-                double zx = 0;
-                double zy = 0;
-                double cX = (x - ANCHO / 2.0) * 4.0 / ANCHO;
-                double cY = (y - ALTO / 2.0) * 4.0 / ANCHO;
-                int iter = 0;
+        ExecutorService executor = Executors.newFixedThreadPool(numWorkers);
+        Future<?>[] futures = new Future[numWorkers];
 
-                while(zx * zx + zy * zy < 4 && iter < MAX_ITER){
-                    double tmp = zx * zx - zy * zy + cX;
-                    zy = 2.0 * zx * zy + cY;
-                    zx = tmp;
-                    iter++;
-                }
+        for (int i = 0; i < numWorkers; i++) {
+            final int workerId = i;
+            futures[i] = executor.submit(() -> {
+                int startY = workerId * (ALTO / numWorkers);
+                int endY = (workerId + 1) * (ALTO / numWorkers);
 
-                if(iter < MAX_ITER){
-                    int color = Color.HSBtoRGB(iter / 256f, 1, iter / (iter + 8f));
-                    image.setRGB(x, y, color);
-                } else {
-                    image.setRGB(x, y, Color.BLACK.getRGB());
+                for (int x = 0; x < ANCHO; x++) {
+                    for (int y = startY; y < endY; y++) {
+                        double zx = 0;
+                        double zy = 0;
+                        double cX = (x - ANCHO / 2.0) * 4.0 / ANCHO;
+                        double cY = (y - ALTO / 2.0) * 4.0 / ANCHO;
+                        int iter = 0;
+
+                        while (zx * zx + zy * zy < 4 && iter < MAX_ITER) {
+                            double tmp = zx * zx - zy * zy + cX;
+                            zy = 2.0 * zx * zy + cY;
+                            zx = tmp;
+                            iter++;
+                        }
+
+                        if (iter < MAX_ITER) {
+                            int color = Color.HSBtoRGB(iter / 256f, 1, iter / (iter + 8f));
+                            image.setRGB(x, y, color);
+                        } else {
+                            image.setRGB(x, y, Color.BLACK.getRGB());
+                        }
+                    }
                 }
+            });
+        }
+
+        for (Future<?> future : futures) {
+            try {
+                future.get();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+
+        executor.shutdown();
+
+        // Pintar la imagen en el panel
+        Graphics g = getGraphics();
         g.drawImage(image, 0, 0, this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // No es necesario pintar nada aquí cuando se usa concurrencia
     }
 }
