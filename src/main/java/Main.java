24be3826a1652c851bdf3575030e62b74c02a8c2
anagd <andelamo@myuax.es>
2023-10-13 09:47:17 +0200

    Mandelbrot concurrente2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
+        SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Mandelbrot Set");
-            Mandelbrot mandelbrotSet = new Mandelbrot();
-            frame.add(mandelbrotSet);
-
-            //Sppiner para seleccionar el número de trabajadores
-            SpinnerModel spinnerModel = new SpinnerNumberModel(1, 1, 8, 1); // Rango de 1 a 8 trabajadores
-            JSpinner spinner = new JSpinner(spinnerModel);
-            JButton startButton = new JButton("Start");
-
-            startButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    int numWorkers = (int) spinner.getValue();
-                    mandelbrotSet.calculateMandelbrot(numWorkers);
-                }
-            });
-
-            JPanel controlPanel = new JPanel();
-            controlPanel.add(new JLabel("Number of Workers: "));
-            controlPanel.add(spinner);
-            controlPanel.add(startButton);
-
-            frame.add(controlPanel, BorderLayout.NORTH);
-
-            frame.pack();
+            Mandelbrot mandelbrot = new Mandelbrot();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLocationRelativeTo(null);
+            frame.setContentPane(mandelbrot);
+            frame.pack();
             frame.setVisible(true);
+
+            JButton button = new JButton("Calculate");
+            button.addActionListener(e -> mandelbrot.pintaMandelbrot());
+            frame.add(button, BorderLayout.SOUTH);
         });
     }
 }
--- src/main/java/Mandelbrot.java
+++ src/main/java/Mandelbrot.java
@@ -1,40 +1,47 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 public class Mandelbrot extends JPanel {
     private static final int ANCHO = 800;
     private static final int ALTO = 800;
     private static final int MAX_ITER = 1000;
+    private JSpinner spinner;
+    private ForkJoinPool forkJoinPool;
+
 
     public Mandelbrot() {
-        setPreferredSize(new java.awt.Dimension(ANCHO, ALTO));
+        setPreferredSize(new Dimension(ANCHO, ALTO));
+        spinner = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
+        add(spinner);
+        forkJoinPool = new ForkJoinPool();
     }
 
-    public void calculateMandelbrot(int numWorkers) {
+    public void pintaMandelbrot() {
         BufferedImage image = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_INT_RGB);
 
+        int numWorkers = (Integer) spinner.getValue();
         int chunkSize = ALTO / numWorkers;
+        MandelbrotTask task = new MandelbrotTask(image, 0, 0, ANCHO, ALTO, chunkSize);
+        image = forkJoinPool.invoke(task);
 
-        for (int i = 0; i < numWorkers; i++) {
-            int startY = i * chunkSize;
-            int endY = (i + 1) * chunkSize;
-
-            calculateMandelbrotForWorker(image, startY, endY);
-        }
-
-        // Pintar la imagen en el panel
-        Graphics g = getGraphics();
-        g.drawImage(image, 0, 0, this);
+        BufferedImage finalImage = image;
+        SwingUtilities.invokeLater(() -> {
+            Graphics g = getGraphics();
+            g.drawImage(finalImage, 0, 0, this);
+        });
     }
 
-    private void calculateMandelbrotForWorker(BufferedImage image, int startY, int endY) {
-        for (int x = 0; x < ANCHO; x++) {
-            for (int y = startY; y < endY; y++) {
+    private void calculateMandelbrotForWorker(BufferedImage image, int startX, int startY, int width, int height) {
+        for (int x = 0; x < width; x++) {
+            for (int y = startY; y < startY + height; y++) {
                 double zx = 0;
                 double zy = 0;
                 double cX = (x - ANCHO / 2.0) * 4.0 / ANCHO;
-                double cY = (y - ALTO / 2.0) * 4.0 / ANCHO;
+                double cY = (y - ALTO / 2.0) * 4.0 / ALTO;
                 int iter = 0;
 
                 while (zx * zx + zy * zy < 4 && iter < MAX_ITER) {
@@ -50,6 +57,9 @@ public class Mandelbrot extends JPanel {
                 } else {
                     image.setRGB(x, y, Color.BLACK.getRGB());
                 }
+
+                // Imprime información sobre el progreso en la consola
+                System.out.println("Thread ID: " + Thread.currentThread().getId() + ", Calculated line " + (startY + x - 1));
             }
         }
     }
@@ -57,5 +67,56 @@ public class Mandelbrot extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+    }
+
+    private class MandelbrotTask extends RecursiveTask<BufferedImage> {
+        private final BufferedImage image;
+        private final int startX;
+        private final int startY;
+        private final int width;
+        private final int height;
+        private final int chunkSize;
+
+        public MandelbrotTask(BufferedImage image, int startX, int startY, int width, int height, int chunkSize) {
+            this.image = image;
+            this.startX = startX;
+            this.startY = startY;
+            this.width = width;
+            this.height = height;
+            this.chunkSize = chunkSize;
+        }
+
+        @Override
+        protected BufferedImage compute() {
+            if (height <= chunkSize) {
+                calculateMandelbrotForWorker(image, startX, startY, width, height);
+                return image;
+            } else {
+                int midY = height / 2;
+                MandelbrotTask topHalf = new MandelbrotTask(image, startX, startY, width, midY, chunkSize);
+                MandelbrotTask bottomHalf = new MandelbrotTask(image, startX, startY + midY, width, height - midY, chunkSize);
+
+                topHalf.fork();
+                BufferedImage result = bottomHalf.compute();
+                topHalf.join();
+
+                return result;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Mandelbrot Set");
+            Mandelbrot mandelbrot = new Mandelbrot();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setContentPane(mandelbrot);
+            frame.pack();
+            frame.setVisible(true);
+
+            JButton button = new JButton("Calculate");
+            button.addActionListener(e -> mandelbrot.pintaMandelbrot());
+            frame.add(button, BorderLayout.SOUTH);
+        });
     }
 }
